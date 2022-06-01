8dac9fbd8147b39ccb087fe50e4c9e9abe366f0c
Benie <davinci.monalissa@gmail.com>
2022-06-01 16:43:42 +0300

    update urls

--- pool/urls.py
+++ pool/urls.py
@@ -8,9 +8,9 @@ from django.contrib import admin
 urlpatterns = [
     path('admin/', admin.site.urls,name='admin'),
     path('',views.galleries,name='galleries'),
-    path(r'^search/tag/',views.tag_results,name='tag_results'),
-    path(r'^search/category/',views.category_results,name='category_results'),
-    path(r'^search/location/',views.location_results,name='location_results'),
+    path('search/tag/',views.tag_results,name='tag_results'),
+    path('search/category/',views.category_results,name='category_results'),
+    path('search/location/',views.location_results,name='location_results'),
     url(r'^image/(\d+)/$',views.image,name='image')
 ]
 
