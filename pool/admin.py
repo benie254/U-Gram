e04fb5f90ad695ff0eb9098f017d0bf1dcba773f
Benie <davinci.monalissa@gmail.com>
2022-06-01 16:45:13 +0300

    register model classes in the app's admin

--- pool/admin.py
+++ pool/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Image,tag,Category,Location
+
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('tag',)
+
 
 # Register your models here.
+admin.site.register(Image,ImageAdmin)
+admin.site.register(tag)
+admin.site.register(Category)
+admin.site.register(Location)
