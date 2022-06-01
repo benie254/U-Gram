1f3c423681b9eb57c228046619ec5743bf5e694c
Benie <davinci.monalissa@gmail.com>
2022-06-01 16:38:39 +0300

    update project url patterns

--- UGram/urls.py
+++ UGram/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('pool.urls')),
+    path('accounts/',include('django_registration.backends.one_step.urls')),
+    path('accounts/',include('django_registration_complete.backends.activation.urls')),
+    path('accounts/',include('django.contrib.auth.urls')),
+    path('logout/',views.LogoutView.as_view,{"next_page":'/'}),
+    path('tinymce/',include('tinymce.urls')),
 ]
